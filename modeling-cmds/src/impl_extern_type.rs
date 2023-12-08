00d56201bbb4c86a5503a07b171122e080c55798
Adam Chalmers <adam.chalmers@kittycad.io>
2023-12-08 13:26:04 -0600

    Add types, impl CXX interop on enums (#9)

---
+++ modeling-cmds/src/impl_extern_type.rs
@@ -0,0 +1,18 @@
+//! A little macro for easy implementation of cxx::ExternType
+
+/// This tells the c++ interop what the native c++ names are
+/// from https://docs.rs/cxx/latest/src/cxx/extern_type.rs.html
+#[macro_export]
+macro_rules! impl_extern_type {
+    ($([$kind:ident] $($(#[$($attr:tt)*])* $ty:path = $cxxpath:literal)*)*) => {
+        $($(
+            $(#[$($attr)*])*
+            unsafe impl cxx::ExternType for $ty {
+                #[allow(unused_attributes)] // incorrect lint; this doc(hidden) attr *is* respected by rustdoc
+                #[doc(hidden)]
+                type Id = cxx::type_id!($cxxpath);
+                type Kind = cxx::kind::$kind;
+            }
+        )*)*
+    };
+}
