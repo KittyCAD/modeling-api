bbd34788fb1dca8190e9cdf0da6629f965a8f608
Adam Chalmers <adam.chalmers@kittycad.io>
2024-02-20 16:04:48 -0600

    Update twenty-twenty snapshots after engine unit scale change

--- modeling-cmds-macros/src/modeling_cmd_enum.rs
+++ modeling-cmds-macros/src/modeling_cmd_enum.rs
@@ -2,7 +2,6 @@ use syn::{ItemMod, spanned::Spanned};
 use proc_macro2::TokenStream;
 
 pub(crate) fn generate(input: ItemMod) -> TokenStream {
-    // Where in the input source code is this type defined?
-    let span = input.span();
+    let _span = input.span();
     todo!()
 }
