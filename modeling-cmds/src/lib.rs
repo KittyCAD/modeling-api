4c84cc64e1796e92b29d8b5c0fa83b2e7621ce85
Adam Chalmers <adam.chalmers@kittycad.io>
2024-02-22 12:26:16 -0600

    Readme

--- modeling-cmds/src/lib.rs
+++ modeling-cmds/src/lib.rs
@@ -1,4 +1,7 @@
 //! KittyCAD's Modeling API lets you design 3D models.
+//! # Beware
+//! This project does not use semver. We are using 0.1.x for everything. If you use this crate, commit your Cargo.lock to avoid being broken when we publish a new version.
+//! Why? Because we use this primarily for KittyCAD server and clients, where we are on top of all changes.
 
 pub mod base64;
 /// Various coordinate systems.
