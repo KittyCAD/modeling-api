09930b8261729e4ab0707d4a1a5e3c0731ac1068
Adam Chalmers <adam.chalmers@kittycad.io>
2024-02-26 19:18:49 -0600

    EP debugger: More data about API calls (#164)
    * EP debugger: More log messages around API requests
    
    * Debugger shows API request name and args

--- modeling-cmds-macros/src/modeling_cmd_enum.rs
+++ modeling-cmds-macros/src/modeling_cmd_enum.rs
@@ -66,7 +66,7 @@ pub(crate) fn generate(input: ItemMod) -> TokenStream {
             #variants(kittycad_modeling_cmds::each_cmd::#variants),
         )*}
         /// Each modeling command (no parameters or fields).
-        #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+        #[derive(Serialize, Deserialize, Debug, PartialEq, Clone, ::parse_display::Display)]
         pub enum ModelingCmdEndpoint{#(
             #[doc = #docs]
             #variants,
