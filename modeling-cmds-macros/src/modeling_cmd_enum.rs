47a28010140f506aa02be067187d7637bc738419
Adam Chalmers <adam.chalmers@kittycad.io>
2024-03-13 14:11:42 -0500

    Modeling commands: ModelingCmd enum is non_exhaustive by default
    This means adding new commands no longer breaks semver, as clients cannot match exhaustively over it by default.
    
    Clients who do wish to (e.g. the engine) can enable the "exhaustive" feature via Cargo.

--- modeling-cmds-macros/src/modeling_cmd_enum.rs
+++ modeling-cmds-macros/src/modeling_cmd_enum.rs
@@ -61,6 +61,7 @@ pub(crate) fn generate(input: ItemMod) -> TokenStream {
         /// Commands that the KittyCAD engine can execute.
         #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
         #[serde(rename_all = "snake_case", tag = "type")]
+        #[cfg_attr(not(exhaustive), non_exhaustive)]
         pub enum ModelingCmd {#(
             #[doc = #docs]
             #variants(kittycad_modeling_cmds::each_cmd::#variants),
