a25f5e41c4c2390927963f792ed22f8526195006
Serena Gandhi <60444726+gserena01@users.noreply.github.com>
2024-03-18 16:12:06 -0400

    switch from unitlength to lengthunit (#232)
    Co-authored-by: gserena <serena@zoo.dev>

--- modeling-cmds/src/def_enum.rs
+++ modeling-cmds/src/def_enum.rs
@@ -292,7 +292,7 @@ define_modeling_cmd_enum! {
             /// Is the helix rotation clockwise?
             pub is_clockwise: bool,
             /// Length of the helix.
-            pub length: units::UnitLength,
+            pub length: LengthUnit,
         }
 
         /// Enter edit mode
