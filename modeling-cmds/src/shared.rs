91d02ce0e28eb91d0b3883140699fb9e6af6ec0d
gserena <serena@zoo.dev>
2024-05-22 12:14:48 -0400

    fmt

--- modeling-cmds/src/shared.rs
+++ modeling-cmds/src/shared.rs
@@ -417,7 +417,12 @@ pub type Quaternion = Point4d;
 impl Default for Quaternion {
     /// (0, 0, 0, 1)
     fn default() -> Self {
-        Self { x: 0.0, y: 0.0, z: 0.0, w: 1.0 }
+        Self {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+            w: 1.0,
+        }
     }
 }
 
