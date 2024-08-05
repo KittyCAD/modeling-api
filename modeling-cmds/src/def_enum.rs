673302ef3cabc000808ec5333be7632b8389649b
gserena <serena@zoo.dev>
2024-08-05 13:55:46 -0400

    add note on negative padding:

--- modeling-cmds/src/def_enum.rs
+++ modeling-cmds/src/def_enum.rs
@@ -1017,6 +1017,7 @@ define_modeling_cmd_enum! {
             #[serde(default = "default_uuid_vector")]
             pub object_ids: Vec<Uuid>,
             /// How much to pad the view frame by, as a fraction of the object(s) bounding box size.
+            /// Negative padding will crop the view of the object proportionally.
             /// e.g. padding = 0.2 means the view will span 120% of the object(s) bounding box.
             pub padding: f32,
         }
