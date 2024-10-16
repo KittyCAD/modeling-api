f700b7a2bf28e2743c08f203976486d674e3d8cb
gserena <serena@zoo.dev>
2024-10-16 12:54:03 -0400

    update docs for isometric to state proportional padding, add default to zoomtofit padding

--- modeling-cmds/src/def_enum.rs
+++ modeling-cmds/src/def_enum.rs
@@ -1052,6 +1052,7 @@ define_modeling_cmd_enum! {
             /// Negative padding will crop the view of the object proportionally.
             /// e.g. padding = 0.2 means the view will span 120% of the object(s) bounding box,
             /// and padding = -0.2 means the view will span 80% of the object(s) bounding box.
+            #[serde(default = "f32::default")]
             pub padding: f32,
             /// Whether or not to animate the camera movement.
             #[serde(default)]
@@ -1061,7 +1062,10 @@ define_modeling_cmd_enum! {
         /// Fit the view to the scene with an isometric view.
         #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, ModelingCmdVariant)]
         pub struct ViewIsometric {
-            /// How much to pad the view frame by.
+            /// How much to pad the view frame by, as a fraction of the object(s) bounding box size.
+            /// Negative padding will crop the view of the object proportionally.
+            /// e.g. padding = 0.2 means the view will span 120% of the object(s) bounding box,
+            /// and padding = -0.2 means the view will span 80% of the object(s) bounding box.
             #[serde(default = "f32::default")]
             pub padding: f32,
         }
