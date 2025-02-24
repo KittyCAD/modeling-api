cab269792d21e61e8757fe237ddfa4f921de96da
alteous <alteous@outlook.com>
2025-02-24 12:35:16 +0000

    Add DXF as an exportable 2D format (#762)
    There is no corresponding import 2D format yet. This is intended only as an
    export format from the engine. Conversions to/from DXF are not supported yet.
    
    `Export` has been split into two variants:` Export2d` and `Export3d`. For backward compatibility, `Export` is now a deprecated alias for `Export3d`. Similar changes have been introduced for `Import`.

---
+++ modeling-cmds/src/format/dxf.rs
@@ -0,0 +1,36 @@
+/// Export sketches in DXF format.
+pub mod export {
+    use parse_display::{Display, FromStr};
+    use schemars::JsonSchema;
+    use serde::{Deserialize, Serialize};
+
+    /// Export storage.
+    #[derive(
+        Clone, Copy, Debug, Default, Deserialize, Display, Eq, FromStr, Hash, JsonSchema, PartialEq, Serialize,
+    )]
+    #[display(style = "snake_case")]
+    #[serde(rename = "DxfStorage", rename_all = "snake_case")]
+    #[cfg_attr(feature = "ts-rs", derive(ts_rs::TS))]
+    #[cfg_attr(feature = "ts-rs", ts(export_to = "ModelingCmd.ts"))]
+    pub enum Storage {
+        /// Plaintext encoding.
+        ///
+        /// This is the default setting.
+        #[default]
+        Ascii,
+
+        /// Binary encoding.
+        Binary,
+    }
+
+    /// Options for exporting DXF format.
+    #[derive(Clone, Debug, Default, Deserialize, Display, Eq, FromStr, Hash, JsonSchema, PartialEq, Serialize)]
+    #[display("storage: {storage}")]
+    #[serde(rename = "DxfExportOptions")]
+    #[cfg_attr(feature = "ts-rs", derive(ts_rs::TS))]
+    #[cfg_attr(feature = "ts-rs", ts(export_to = "ModelingCmd.ts"))]
+    pub struct Options {
+        /// Export storage.
+        pub storage: Storage,
+    }
+}
