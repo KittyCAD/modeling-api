3ec6888adb3b778299913b511c2606a246a544d0
Mike Farrell <michael.l.farrell@gmail.com>
2025-06-12 11:08:12 -0700

    New endpoint TwistExtrude

--- modeling-cmds/src/ok_response.rs
+++ modeling-cmds/src/ok_response.rs
@@ -49,6 +49,11 @@ define_ok_modeling_cmd_response_enum! {
         pub struct Extrude {
         }
 
+        /// The response from the `TwistExtrude` endpoint.
+        #[derive(Debug, Serialize, Deserialize, Clone, JsonSchema, ModelingCmdOutput)]
+        pub struct TwistExtrude {
+        }
+
         /// The response from the `Sweep` endpoint.
         #[derive(Debug, Serialize, Deserialize, Clone, JsonSchema, ModelingCmdOutput)]
         pub struct Sweep {
