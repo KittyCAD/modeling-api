648f90d36758d391538b7a1ae1ab7b5a04178297
ben <ben.crabbe@hotmail.co.uk>
2026-01-07 11:32:39 +0000

    add body_type to loft

--- modeling-cmds/src/def_enum.rs
+++ modeling-cmds/src/def_enum.rs
@@ -309,6 +309,9 @@ define_modeling_cmd_enum! {
             pub base_curve_index: Option<u32>,
             /// Tolerance
             pub tolerance: LengthUnit,
+            /// Should this extrude create a solid body or a surface?
+            #[serde(default)]
+            pub body_type: BodyType,
         }
 
 
