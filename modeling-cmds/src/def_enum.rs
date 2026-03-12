fbd5b34a5c260ee1728d029045b6cd6757d80f6f
Adam Chalmers <adam.chalmers@zoo.dev>
2026-03-12 10:13:43 -0500

    Non-destructive, partial extrudes/revolves/sweeps etc
    This proposes a new way to surface extrude/revolve/sweep for sketch 2.
    
    Instead of extruding an entire sketch (solid2D) at once, we instead
    take a list of segments and extrude each of those separately.
    
    Each segment has a (segment ID, new body ID) pair, so the segment IDs
    are NOT absorbed into new body IDs. The sketch and all its segments are
    not modified.

--- modeling-cmds/src/def_enum.rs
+++ modeling-cmds/src/def_enum.rs
@@ -128,8 +128,17 @@ define_modeling_cmd_enum! {
         #[cfg_attr(not(feature = "unstable_exhaustive"), non_exhaustive)]
         pub struct Extrude {
             /// Which sketch to extrude.
-            /// Must be a closed 2D solid.
+            /// Must be a 2D sketch.
             pub target: ModelingCmdId,
+            /// If given, extrude each of these segments into a separate body with the given ID.
+            #[serde(default, skip_serializing_if = "Option::is_none")]
+            pub segments: Option<Vec<Uuid>>,
+            /// When two collinear segments are extruded, what should happen?
+            /// If true, creates a body with two separate surfaces.
+            /// If false, creates a body with one surface, spanning both collinear segments.
+            #[serde(default)]
+            #[builder(default)]
+            pub keep_seams: bool,
             /// How far off the plane to extrude
             pub distance: LengthUnit,
             /// Which IDs should the new faces have?
@@ -169,6 +178,15 @@ define_modeling_cmd_enum! {
             /// Which sketch to extrude.
             /// Must be a closed 2D solid.
             pub target: ModelingCmdId,
+            /// If given, extrude each of these segments into a separate body with the given ID.
+            #[serde(default, skip_serializing_if = "Option::is_none")]
+            pub segments: Option<Vec<Uuid>>,
+            /// When two collinear segments are extruded, what should happen?
+            /// If true, creates a body with two surfaces.
+            /// If false, creates a body with one surface, spanning both collinear segments.
+            #[serde(default)]
+            #[builder(default)]
+            pub keep_seams: bool,
             /// Reference to extrude to.
             /// Extrusion occurs along the target's normal until it is as close to the reference as possible.
             pub reference: ExtrudeReference,
@@ -202,6 +220,15 @@ define_modeling_cmd_enum! {
             /// Which sketch to extrude.
             /// Must be a closed 2D solid.
             pub target: ModelingCmdId,
+            /// If given, extrude each of these segments into a separate body with the given ID.
+            #[serde(default, skip_serializing_if = "Option::is_none")]
+            pub segments: Option<Vec<Uuid>>,
+            /// When two collinear segments are extruded, what should happen?
+            /// If true, creates a body with two surfaces.
+            /// If false, creates a body with one surface, spanning both collinear segments.
+            #[serde(default)]
+            #[builder(default)]
+            pub keep_seams: bool,
             /// How far off the plane to extrude
             pub distance: LengthUnit,
             /// Which IDs should the new faces have?
@@ -239,6 +266,15 @@ define_modeling_cmd_enum! {
             /// Which sketch to sweep.
             /// Must be a closed 2D solid.
             pub target: ModelingCmdId,
+            /// If given, extrude each of these segments into a separate body with the given ID.
+            #[serde(default, skip_serializing_if = "Option::is_none")]
+            pub segments: Option<Vec<Uuid>>,
+            /// When two collinear segments are extruded, what should happen?
+            /// If true, creates a body with two surfaces.
+            /// If false, creates a body with one surface, spanning both collinear segments.
+            #[serde(default)]
+            #[builder(default)]
+            pub keep_seams: bool,
             /// Path along which to sweep.
             pub trajectory: ModelingCmdId,
             /// If true, the sweep will be broken up into sub-sweeps (extrusions, revolves, sweeps) based on the trajectory path components.
@@ -271,6 +307,15 @@ define_modeling_cmd_enum! {
             /// Which sketch to revolve.
             /// Must be a closed 2D solid.
             pub target: ModelingCmdId,
+            /// If given, extrude each of these segments into a separate body with the given ID.
+            #[serde(default, skip_serializing_if = "Option::is_none")]
+            pub segments: Option<Vec<Uuid>>,
+            /// When two collinear segments are extruded, what should happen?
+            /// If true, creates a body with two surfaces.
+            /// If false, creates a body with one surface, spanning both collinear segments.
+            #[serde(default)]
+            #[builder(default)]
+            pub keep_seams: bool,
             /// The origin of the extrusion axis
             pub origin: Point3d<LengthUnit>,
             /// The axis of the extrusion (taken from the origin)
@@ -422,6 +467,15 @@ define_modeling_cmd_enum! {
             /// Which sketch to revolve.
             /// Must be a closed 2D solid.
             pub target: ModelingCmdId,
+            /// If given, extrude each of these segments into a separate body with the given ID.
+            #[serde(default, skip_serializing_if = "Option::is_none")]
+            pub segments: Option<Vec<Uuid>>,
+            /// When two collinear segments are extruded, what should happen?
+            /// If true, creates a body with two surfaces.
+            /// If false, creates a body with one surface, spanning both collinear segments.
+            #[serde(default)]
+            #[builder(default)]
+            pub keep_seams: bool,
             /// The edge to use as the axis of revolution, must be linear and lie in the plane of the solid
             #[serde(default, skip_serializing_if = "Option::is_none")]
             pub edge_id: Option<Uuid>,
--- modeling-cmds/src/shared.rs
+++ modeling-cmds/src/shared.rs
@@ -1910,6 +1910,45 @@ pub struct SurfaceEdgeReference {
     pub edges: Vec<FractionOfEdge>,
 }
 
+/// List of bodies that were created by an operation.
+#[derive(Builder, Debug, Clone, PartialEq, Serialize, Deserialize, JsonSchema)]
+#[cfg_attr(feature = "ts-rs", derive(ts_rs::TS))]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+#[cfg_attr(feature = "ts-rs", ts(export_to = "ModelingCmd.ts"))]
+#[cfg_attr(not(feature = "unstable_exhaustive"), non_exhaustive)]
+pub struct BodiesCreated {
+    /// All bodies created by this operation.
+    pub bodies: Vec<BodyCreated>,
+}
+
+/// Details of a body that was created.
+#[derive(Builder, Debug, Clone, PartialEq, Serialize, Deserialize, JsonSchema)]
+#[cfg_attr(feature = "ts-rs", derive(ts_rs::TS))]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+#[cfg_attr(feature = "ts-rs", ts(export_to = "ModelingCmd.ts"))]
+#[cfg_attr(not(feature = "unstable_exhaustive"), non_exhaustive)]
+pub struct BodyCreated {
+    /// The body's ID.
+    pub id: Uuid,
+    /// Surfaces this body contains.
+    pub surfaces: Vec<SurfaceCreated>,
+}
+
+/// Details of a surface that was created under some body.
+#[derive(Builder, Debug, Clone, PartialEq, Serialize, Deserialize, JsonSchema)]
+#[cfg_attr(feature = "ts-rs", derive(ts_rs::TS))]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+#[cfg_attr(feature = "ts-rs", ts(export_to = "ModelingCmd.ts"))]
+#[cfg_attr(not(feature = "unstable_exhaustive"), non_exhaustive)]
+pub struct SurfaceCreated {
+    /// The surface's ID.
+    pub id: Uuid,
+    /// Which number face of the parent body is this?
+    pub primitive_face_index: u32,
+    /// Which segment IDs was this surface swept from?
+    pub from_segments: Vec<Uuid>,
+}
+
 fn one() -> f32 {
     1.0
 }
