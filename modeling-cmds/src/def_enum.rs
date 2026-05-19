ba67c72cb74ed4d9cf8b82e2c1bfa09fb652ad25
gserena <serena@zoo.dev>
2026-05-19 12:10:51 -0400

    add draft to extrude def

--- modeling-cmds/src/def_enum.rs
+++ modeling-cmds/src/def_enum.rs
@@ -134,6 +134,11 @@ define_modeling_cmd_enum! {
             pub target: ModelingCmdId,
             /// How far off the plane to extrude
             pub distance: LengthUnit,
+            /// What draft angle should be used in this extrusion?
+            /// Negative values indicate an outward draft, 
+            /// while positive values indicate an inward draft
+            #[serde(default)]
+            pub draft_angle: f64,
             /// Which IDs should the new faces have?
             /// If this isn't given, the engine will generate IDs.
             #[serde(default)]
