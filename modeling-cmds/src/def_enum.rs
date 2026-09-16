f6b527198fee33b44ec9136805120883df4369a7
Adam Chalmers <adam.chalmers@zoo.dev>
2026-09-16 14:18:21 -0500

    New command SetKclVersion

--- modeling-cmds/src/def_enum.rs
+++ modeling-cmds/src/def_enum.rs
@@ -2849,6 +2849,17 @@ define_modeling_cmd_enum! {
             /// Which path to query
             pub path_id: ModelingCmdId,
         }
+
+        /// Sets the KCL Version used by the engine.
+        #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, JsonSchema, ModelingCmdVariant, Builder)]
+        #[cfg_attr(feature = "ts-rs", derive(ts_rs::TS))]
+        #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+        #[cfg_attr(feature = "ts-rs", ts(export_to = "ModelingCmd.ts"))]
+        #[cfg_attr(not(feature = "unstable_exhaustive"), non_exhaustive)]
+        pub struct SetKclVersion {
+            /// Which KCL version the following commands should be executed with.
+            pub kcl_version: crate::KclVersion,
+        }
     }
 }
 
